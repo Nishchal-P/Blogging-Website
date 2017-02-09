63f4f1d24f439e60dc4f33aebb56bcfd1c2fc498
AkhilPowar <akhilpowar16@yahoo.com>
2017-02-09 22:06:03 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,35 +1,33 @@
 <?php
-define('DB_HOST', "localhost");
-define('DB_NAME', "login");
-define('DB_USER',"root");
-define('DB_PASSWORD',"");
-
-$con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysqli_error($con));
-$db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysqli_error($con));
-
-
-
-function SignIn()
-{
-session_start();
-if(!empty($_POST['user'])
-{       global $con;
-	$query = mysqli_query( $con,"SELECT *  FROM registered where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error($con));
-	$row = mysqli_fetch_array($query) ;
-	if(!empty($row['userName']) AND !empty($row['pass']))
-	{
-		$_SESSION['userName'] = $row['pass'];
-		echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+include("config.php");
+function NewUser() {
+   global $con;
+   $firstname = $_POST['fname'];
+   $lastname= $_POST['lname'];
+   $age=$_POST['age'];
+   $gender=$_POST['gender'];
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+   $username=$_POST['username'];
+   $query = "INSERT INTO user (firstname,lastname,age,gender,email,password,username) VALUES ('$firstname','$lastname','$age','$gender','$email','$password','$username')";
+   $data = mysqli_query ($con,$query)or die(mysql_error($con));
+   if($data) {
+      echo "YOUR REGISTRATION IS COMPLETED...";
+   }
 }
-	else
-	{
-		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
-	}
+function SignUp(){
+   global $con;
+   if(!empty($_POST['username'])){
+      $query = mysqli_query($con,"SELECT * FROM user WHERE username = '$_POST[username]' AND password = '$_POST[password]'") or die(mysqli_error($con));
+      if(!$row = mysqli_fetch_array($con,$query) or die(mysqli_error($con))){
+         newuser();
+      }
+      else{
+         echo "Sorry! It seems this name is already taken!";
+      }
+   }
 }
+if(isset($_POST['submit'])){
+   SignUp();
 }
-if(isset($_POST['submit']))
-{
-	SignIn();
-}
-
 ?>
