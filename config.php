361aa280836eacd3ba46b467c2beb21a81bc8ad7
AkhilPowar <akhilpowar16@yahoo.com>
2017-02-09 22:22:31 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+   define('DB_HOST', 'localhost');
+   define('DB_NAME', 'blog');
+   define('DB_USER','blog_user');
+   define('DB_PASSWORD','blog_user');
+   $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error($con));
+   $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error($con));
+?>
