1b248abf9b371d128be930c46bb99e074073efd7
AkhilPowar <akhilpowar16@yahoo.com>
2017-02-11 16:54:47 +0530

    Stable version

--- sign-up.php
+++ sign-up.php
@@ -1,33 +1,33 @@
 <?php
 include("config.php");
-function NewUser() {
-   global $con;
-   $firstname = $_POST['fname'];
-   $lastname= $_POST['lname'];
-   $age=$_POST['age'];
-   $gender=$_POST['gender'];
-   $email = $_POST['email'];
-   $password = $_POST['password'];
-   $username=$_POST['username'];
-   $query = "INSERT INTO user (firstname,lastname,age,gender,email,password,username) VALUES ('$firstname','$lastname','$age','$gender','$email','$password','$username')";
-   $data = mysqli_query ($con,$query)or die(mysql_error($con));
-   if($data) {
-      echo "YOUR REGISTRATION IS COMPLETED...";
-   }
+function NewUser(){
+    global $con;
+    $firstname = $_POST['fname'];
+    $lastname = $_POST['lname'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $password = $_POST['pass'];
+    $username = $_POST['user'];
+    $query = "INSERT INTO user VALUES ('$username', '$password', '$firstname', '$lastname', '$age', '$gender', '$email')";
+    $data = mysqli_query($con, $query)or die(mysql_error($con));
+    if($data){
+        echo "Sign-Up Successful!";
+    }
 }
 function SignUp(){
-   global $con;
-   if(!empty($_POST['username'])){
-      $query = mysqli_query($con,"SELECT * FROM user WHERE username = '$_POST[username]' AND password = '$_POST[password]'") or die(mysqli_error($con));
-      if(!$row = mysqli_fetch_array($con,$query) or die(mysqli_error($con))){
-         newuser();
-      }
-      else{
-         echo "Sorry! It seems this name is already taken!";
-      }
-   }
+    global $con;
+    if(!empty($_POST['user'])){
+        $query = mysqli_query($con, "SELECT * FROM user WHERE username='$_POST[user]'") or die(mysql_error($con));
+        if(!$row = mysqli_fetch_array($query) or die(mysql_error($con))){
+            NewUser();
+        }
+        else{
+            echo "Sorry! It seems this name is already taken!";
+        }
+    }
 }
 if(isset($_POST['submit'])){
-   SignUp();
+    SignUp();
 }
 ?>
